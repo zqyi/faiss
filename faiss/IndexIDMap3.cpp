3bea6c6822ddec35643c697ee3a41bb1bcabb0ed
zhengqianyi.zqy <zhengqianyi.zqy@alibaba-inc.com>
2024-07-15 02:38:14 +0000

    feat(idmap3): format

--- faiss/IndexIDMap3.cpp
+++ faiss/IndexIDMap3.cpp
@@ -1,16 +1,16 @@
 #include <faiss/IndexIDMap3.h>
 
+#include <string.h>
 #include <algorithm>
-#include <cstddef>
 #include <cinttypes>
+#include <cstddef>
 #include <stdexcept>
 #include <vector>
-#include <string.h>
 
 #include "faiss/MetricType.h"
 #include "faiss/impl/FaissAssert.h"
 
-namespace faiss{
+namespace faiss {
 /*****************************************************
  * IndexIDMap3 implementation
  *******************************************************/
@@ -19,13 +19,12 @@ template <typename IndexT>
 IndexIDMap3Template<IndexT>::IndexIDMap3Template(IndexT* index)
         : IndexIDMapTemplate<IndexT>(index) {}
 
-
 template <typename IndexT>
 void IndexIDMap3Template<IndexT>::add_with_ids(
         idx_t n,
         const typename IndexT::component_t* x,
         const idx_t* xids) {
-    size_t prev_ntotal = this->ntotal;    
+    size_t prev_ntotal = this->ntotal;
     IndexIDMapTemplate<IndexT>::add_with_ids(n, x, xids);
     for (size_t i = prev_ntotal; i < this->ntotal; i++) {
         rev_map[this->id_map[i]].push_back(i);
@@ -43,8 +42,8 @@ void IndexIDMap3Template<IndexT>::check_consistency() const {
     FAISS_THROW_IF_NOT(rev_map_size == this->id_map.size());
 
     // check value
-    for (const auto& [doc_id, inner_id_vec] : rev_map){
-        for (const idx_t& inner_id : inner_id_vec){
+    for (const auto& [doc_id, inner_id_vec] : rev_map) {
+        for (const idx_t& inner_id : inner_id_vec) {
             FAISS_THROW_IF_NOT(this->id_map[inner_id] == doc_id);
         }
     }
@@ -82,7 +81,7 @@ void IndexIDMap3Template<IndexT>::reconstruct(
         idx_t key,
         typename IndexT::component_t* recons) const {
     try {
-        const auto &inner_id_vec = rev_map.at(key);
+        const auto& inner_id_vec = rev_map.at(key);
         if (inner_id_vec.size() == 0) {
             FAISS_THROW_FMT("key %" PRId64 " not found", key);
         }
@@ -94,7 +93,8 @@ void IndexIDMap3Template<IndexT>::reconstruct(
 
 template <typename IndexT>
 void IndexIDMap3Template<IndexT>::get_reconstruct_multi_nums(
-    idx_t key, size_t& n) const {
+        idx_t key,
+        size_t& n) const {
     try {
         const auto& inner_ids = rev_map.at(key);
         size_t inner_n = inner_ids.size();
@@ -107,11 +107,11 @@ void IndexIDMap3Template<IndexT>::get_reconstruct_multi_nums(
     }
 }
 
-
 template <typename IndexT>
 void IndexIDMap3Template<IndexT>::reconstruct_multi(
-    idx_t key,
-    component_t*& recons, size_t& n) const {
+        idx_t key,
+        component_t*& recons,
+        size_t& n) const {
     try {
         const auto& inner_ids = rev_map.at(key);
 
@@ -119,14 +119,18 @@ void IndexIDMap3Template<IndexT>::reconstruct_multi(
         if (inner_n == 0) {
             FAISS_THROW_FMT("key %" PRId64 " not found", key);
         }
-        
-        if (recons == nullptr){
-            recons = new component_t[inner_n*this->index->d];
-            std::fill_n(recons, inner_n*this->index->d, static_cast<component_t>(0));
+
+        if (recons == nullptr) {
+            recons = new component_t[inner_n * this->index->d];
+            std::fill_n(
+                    recons,
+                    inner_n * this->index->d,
+                    static_cast<component_t>(0));
         }
 
-        for(size_t i=0;i<inner_n;i++){
-            this->index->reconstruct(inner_ids[i], &recons[i*(this->index->d)]);
+        for (size_t i = 0; i < inner_n; i++) {
+            this->index->reconstruct(
+                    inner_ids[i], &recons[i * (this->index->d)]);
         }
 
         n = inner_n;
@@ -138,4 +142,4 @@ void IndexIDMap3Template<IndexT>::reconstruct_multi(
 template struct IndexIDMap3Template<Index>;
 template struct IndexIDMap3Template<IndexBinary>;
 
-}// namespace faiss
+} // namespace faiss
